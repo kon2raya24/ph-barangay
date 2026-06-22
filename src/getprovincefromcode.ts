f7ae4a5aac4951814531b711884c35abaf5de3a0
kon2raya24 <konturaya@gmail.com>
2026-06-22 09:02:12 +0000

    docs: add JSDoc comments to getprovincefromcode.ts

--- src/getprovincefromcode.ts
+++ src/getprovincefromcode.ts
@@ -1,3 +1,7 @@
+/**
+ * getProvinceFromCode
+ * @param code
+ */
 export function getProvinceFromCode(code: string): string {
   if (!validateBarangayCode(code)) throw new Error("Invalid barangay code");
   return code.slice(0, 4) + "000000";
